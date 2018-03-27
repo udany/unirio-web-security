a6ef1f9dff84489f54feaa5bbd5e5c1c1f80b9eb
udany <long.eagle.goodies@gmail.com>
2018-03-27 11:27:51 -0300

    Cleaning up ans setting basics

--- MySite/app.js
+++ MySite/app.js
@@ -1,17 +1,33 @@
 let express = require('express');
 let path = require('path');
+let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-require('../util/General');
+let session = require('express-session');
+
+require('./util/General');
 
 let app = express();
 
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'static')));
+
+/// =SESSION
+let cookieSecret = 'keyboard cat';
+app.use(session({
+    secret: cookieSecret,
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        maxAge: 60000,
+        httpOnly: true
+    }
+}));
 
 // Add all routes
 let glob = require('glob');
@@ -25,9 +41,9 @@ for (let file of routeFiles) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 module.exports = app;
